1773cca8f2c8b84a4ddeeaca4d8ecb89d4298d58
Lance Perez <142560197+lanceperez001@users.noreply.github.com>
2024-02-23 18:08:03 +0000

    Fri, Feb 23, 2024, 10:08 AM -08:00

--- js/search.js
+++ js/search.js
@@ -1,5 +1,6 @@
 btn = document.querySelector('.btn')
-search = document.querySelector('.Search')
+search = document.querySelector('.search')
+
 
 btn.addEventListener('click', () => {
     search.classList.toggle('active')

149c07a59af994ce2e1dd4d65f4c9efa109909ef
Lance Perez <142560197+lanceperez001@users.noreply.github.com>
2024-02-27 08:55:25 +0000

    Tue, Feb 27, 2024, 12:55 AM -08:00

--- js/loading.js
+++ js/loading.js
@@ -1,5 +1,8 @@
 loadText = document.querySelector('.loading-text')
+bg = document.querySelector('.bg')
 
-load = 4
+load = 0
 
-loadText.innerText = `${load}` 
+loadText.innerText = `${load}%`
+loadText.style.opacity = 1
+bg.style.filter = "blur(10px)"

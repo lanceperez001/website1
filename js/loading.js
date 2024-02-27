d1fc47bd0ff64ebdeb3fb6c3f00ffb8b278e7618
Lance Perez <142560197+lanceperez001@users.noreply.github.com>
2024-02-27 09:00:57 +0000

    Tue, Feb 27, 2024, 1:00 AM -08:00

--- js/loading.js
+++ js/loading.js
@@ -3,13 +3,18 @@ bg = document.querySelector('.bg')
 
 load = 0
 
-blurring()
+int = setInterval(blurring, 30)
 
 
 
 function blurring() {
     load++
+
+    if (load > 99){
+        clearInterval(int)
+    }
     loadText.innerText = `${load}%`
-    loadText.style.opacity = 1
-    bg.style.filter = "blur(10px)"
+    loadText.style.opacity = 1 - load/100
+
+    bg.style.filter = `blur(${30-(load/100)*30}px)`
 }

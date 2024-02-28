4aa04b432cee4af196df3ea3f98381a77040a605
Lance Perez <142560197+lanceperez001@users.noreply.github.com>
2024-02-28 17:02:44 +0000

    Wed, Feb 28, 2024, 9:02 AM -08:00

--- js/sound.js
+++ js/sound.js
@@ -1,1 +1,9 @@
-sounds = ['applause]
+sounds = ['applause', 'boo', 'gasp', 'tada', 'vicotry', 'wrong']
+
+sounds.ForEach(sound => {
+    btn = document.createElement('button')
+    btn.classList.add('btn')
+    btn.innerText=sound
+
+    document.getElementById('buttons').appendChild(btn)
+})

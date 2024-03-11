370159d0fdc488b6f1c0fe42a3acbca322097961
Lance Perez <142560197+lanceperez001@users.noreply.github.com>
2024-03-11 17:03:20 +0000

    Mon, Mar 11, 2024, 10:03 AM -07:00

--- js/landingPage.js
+++ js/landingPage.js
@@ -1,8 +1,23 @@
 toggle = document.getElementById('toggle')
 open = document.getElementById('open')
-model = document.getElementById('model')
+modal = document.getElementById('modal')
+close = document.getEelemntById('close')
 
 
 toggle.addEventListener('click', () => {
     documnet.bodyclassList.toggle('show-nav')
+})
+
+open.addEventListener('click', () => {
+    modal.ClassList.add('show-modal')
+})
+
+close.addEventListener('click', () => {
+    modal.ClassList.remove('show-modal')
+})
+
+close.addEventListener('click', (e) => {
+    if (e.target == modal) {
+        modal.classList.remove('show-modal')
+    }
 })
